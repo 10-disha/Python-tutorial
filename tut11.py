90845b60fbd970f6ab565f89f0b05a0cc6d50a3f
10-disha <jayswaldisha5@gmail.com>
2021-06-10 09:12:53 +0530

    dictionary quiz

---
+++ tut11.py
@@ -0,0 +1,6 @@
+# create a dictionary & take input  from the user & return the right meaning of word from the dictionary
+
+animals={"dog":"street animal here","lion":"wild animal in forest","elephant":"huge land animal","whale":"largest sea mammal"}
+print("search for:")
+inp=input()
+print(animals[inp])
