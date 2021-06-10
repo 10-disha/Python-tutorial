83dcec69dd21f6e747cf0ca70275dfd66f25e29d
10-disha <jayswaldisha5@gmail.com>
2021-06-10 09:41:24 +0530

    else if

---
+++ quiz2.py
@@ -0,0 +1,9 @@
+print("enter age:")
+age=int(input())
+
+if 100>age>18:
+    print("u can get")
+elif age<18:
+    print("no")
+else:
+    print("come pyhsically")        
---
+++ tut13.py
@@ -0,0 +1,17 @@
+# var1 =6
+# var2=33
+# var3=int(input())
+
+# if var3>var2:
+#     print("greater")
+# elif var3==var2:
+#     print("equal")    
+# else:
+#     print("lesser")    
+
+# elif will only be executed if 1st statement is wrong ,it works like a break statement for previous one
+
+list1 = [3,4,5]
+print(4 not in list1)
+if 5 in list1:
+    print("yes in list")
