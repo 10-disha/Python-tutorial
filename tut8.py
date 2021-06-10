8f345fefddb2ce97c7164148c8ff982d1f1fec7a
10-disha <jayswaldisha5@gmail.com>
2021-06-10 08:25:33 +0530

    string

--- tut8.py
+++ tut8.py
@@ -4,4 +4,17 @@
 
 mystr = "harry is a good boy"
 print(len(mystr))
-print(mystr[0:5])
+# this will skip alternate will get print
+print(mystr[::2])
+# 3rd colun will skip characters
+# negative indices start from reverse 
+
+print(type(mystr))
+print(mystr.isalnum())
+# isalnum comes false as there are spaces in between string
+
+print(mystr.endswith("bo"))
+print(mystr.count("o"))
+print(mystr.capitalize())
+print(mystr.upper())
+print(mystr.replace("is","are"))
